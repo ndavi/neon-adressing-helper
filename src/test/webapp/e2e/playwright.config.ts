2ebe6928c9df39b2f55397c2af73fd50472df534
Nicolas David <nico01.david@gmail.com>
2025-12-25 23:04:08 +0100

    test(e2e): disable automatic opening of playwright report and remove ui flag from default e2e script

--- src/test/webapp/e2e/playwright.config.ts
+++ src/test/webapp/e2e/playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['list'], ['html', { outputFolder: '../../../../target/playwright-report/e2e-tests' }]],
+  reporter: [['list'], ['html', { outputFolder: '../../../../target/playwright-report/e2e-tests', open: 'never' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */

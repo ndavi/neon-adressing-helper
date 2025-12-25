43fd5af6e86619ae807951f088b68eb8f5f08a49
Nicolas David <nico01.david@gmail.com>
2025-12-25 20:54:14 +0100

    Apply module: tikui

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,4 +1,6 @@
 module.exports = {
+  '*.{css,scss}': ['stylelint --fix --allow-empty-input', 'prettier --write'],
+  '*.pug': 'prettier --write',
   '{src/**/,}*.{ts,vue}': ['eslint --fix', 'prettier --write'],
   '*.{md,json*,yml,html,css,scss,java,xml,feature}': ['prettier --write'],
 };

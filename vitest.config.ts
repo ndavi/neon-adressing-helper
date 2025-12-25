e6c59b13c7b2a4cb944826c14a4cc11e0504e567
Nicolas David <nico01.david@gmail.com>
2025-12-25 20:51:59 +0100

    Apply module: vue-core

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,10 @@
 
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { configDefaults, defineConfig } from 'vitest/config';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [vue(), tsconfigPaths()],
   test: {
     reporters: ['verbose', 'vitest-sonar-reporter'],
     outputFile: {
@@ -13,7 +14,7 @@ export default defineConfig({
     globals: true,
     logHeapUsage: true,
     maxWorkers: 2,
-    environment: 'node',
+    environment: 'jsdom',
     cache: false,
     include: ['src/test/webapp/unit/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
     coverage: {
@@ -23,7 +24,7 @@ export default defineConfig({
         100: true,
       },
       include: ['src/main/webapp/**/*.ts?(x)'],
-      exclude: [...(configDefaults.coverage.exclude as string[])],
+      exclude: [...(configDefaults.coverage.exclude as string[]), 'src/main/webapp/app/main.ts', 'src/main/webapp/app/injections.ts', 'src/main/webapp/**/*.component.ts'],
       provider: 'istanbul',
       reportsDirectory: 'target/test-results/',
       reporter: ['html', 'json', 'json-summary', 'text', 'text-summary', 'lcov', 'clover'],

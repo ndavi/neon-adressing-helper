df5b7a5b6bbf3dae92c8c403b6647b72c53683ac
Nicolas David <nico01.david@gmail.com>
2025-12-26 18:48:32 +0100

    chore: remove dummy tesrt

--- src/test/webapp/unit/Dummy.spec.ts
+++
@@ -1,8 +0,0 @@
-import { describe } from 'vitest';
-
-// This dummy test can be removed as soon as you have another test. It's here only to keep coverage analysis working.
-describe('Dummy test', () => {
-  it('should pass', () => {
-    expect(true).toBe(true);
-  });
-});

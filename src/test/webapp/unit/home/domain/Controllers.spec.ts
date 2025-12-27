864f11b94039bf235e3e6ce64cf6f5609ebf841f
Nicolas David <nico01.david@gmail.com>
2025-12-27 11:06:01 +0100

    test: simplify Controllers spec to avoid testing Controller logic

--- src/test/webapp/unit/home/domain/Controllers.spec.ts
+++ src/test/webapp/unit/home/domain/Controllers.spec.ts
@@ -56,8 +56,6 @@ describe('Controllers Domain', () => {
     expect(duplicatedControllers.values).toHaveLength(3);
 
     const duplicated = controllerAt(duplicatedControllers, 2);
-    expect(duplicated.universe).toBe(62);
-    expect(duplicated.outputs).toHaveLength(3);
     expect(duplicated.index).toBe(2);
   });
 
